1104a754f8a78f28827016fb9a9431159070aa53
JoaquinBattilana <jbattilana@itba.edu.ar>
2017-07-07 23:50:34 -0300

    Update backend.h

--- backend.h
+++ backend.h
@@ -2,6 +2,13 @@
 #ifndef BACKEND_H
 #define BACKEND_H
 
+
 typedef struct censoCDT * censoADT;
+
+void departamentoCsv(censoADT);
+void provinciaCsv(censoADT);
+void analfabetismoCsv(censoADT);
+void add(censoADT,int,int,int, int, char*);
+void imprimirProvincia(censoADT, int);
 
 #endif
